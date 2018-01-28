5ad5076da9535bb372230b8e690a1149c618481c
Jon Linnell <linnell.jon@gmail.com>
2018-01-28 08:35:46 +0000

    Added demo BS4

--- src/components/ui/App.jsx
+++ src/components/ui/App.jsx
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='col-md-6 offset-md-3 mt-2 card card-body bg-light'>
         <h1>Hello world!</h1>
         <div id='errors'>
-          {this.props.errors}
+          {this.props.errors.map((error, i) => <p key={i} className='alert alert-danger'>{error}</p>)}
         </div>
       </div>
     )
