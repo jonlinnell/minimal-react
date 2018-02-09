617660e18a451f59332c39cffd1a0b78d8b3ccf7
Jon Linnell <linnell.jon@gmail.com>
2018-02-09 07:42:48 +0000

    Login jumbotron

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux'
 import App from '../ui/App'
 
 const mapStateToProps = state => ({
-  errors: state.errors
+  errors: state.errors,
+  auth: state.auth
 })
 
 export default connect(mapStateToProps)(App)
--- src/components/ui/App.jsx
+++ src/components/ui/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { Route, Router } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { faSignInAlt } from '@fortawesome/fontawesome-free-solid'
 
 import PrivateRoute from '../misc/PrivateRoute'
 
@@ -10,9 +13,16 @@ import Links from '../containers/Links'
 
 import history from '../../history'
 
-const Home = () =>
-  <div>
-    <p>This is the test home element</p>
+const DefaultHome = () =>
+  <div className='jumbotron col-sm-8 offset-sm-2'>
+    <h1 className='display-4'>Login</h1>
+    <p className='lead'>Please login to Ricochet with your account details to view and manage links, and see statistics and usage data.</p>
+    <LinkContainer to='/login'>
+      <button className='btn btn-primary'>
+        <FontAwesomeIcon className='mr-2' icon={ faSignInAlt } />
+        Login
+      </button>
+    </LinkContainer>
   </div>
 
 class App extends Component {
@@ -25,7 +35,7 @@ class App extends Component {
             {this.props.errors.map((error, i) => <ClientError key={i} index={i} error={error} />)}
           </div>
 
-          <Route exact path='/' component={Home} />
+          <Route exact path='/' component={this.props.auth.isAuthenticated ? null : DefaultHome} />
           <Route path='/login' component={Login} />
           <PrivateRoute path='/links' component={Links} />
         </div>
