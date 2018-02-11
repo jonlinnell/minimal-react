102a366c2afe7ea6906ebaef5e5d9ba8ae89e329
Jon Linnell <linnell.jon@gmail.com>
2018-02-11 10:35:35 +0000

    Partially rethemed nav and body

--- src/components/containers/Navbar.js
+++ src/components/containers/Navbar.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { logout } from '../../store/actions'
 import Navbar from '../ui/Navbar'
 
+import '../../styles/Navbar.css'
+
 const mapStateToProps = state => ({
   auth: state.auth
 })
