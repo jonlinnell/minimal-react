8f6404b812de3e43fab2895426bf49c1d0974e95
Jon Linnell <linnell.jon@gmail.com>
2018-02-17 10:27:02 +0000

    Made hostResolver NODE_ENV specific (http/s)

--- src/lib/hostResolver.js
+++ src/lib/hostResolver.js
@@ -1,3 +1,3 @@
-const hostResolver = () => `http://${process.env.HOST}:${process.env.PORT}`
+const hostResolver = () => `${(process.env.NODE_ENV === 'production' ? 'https' : 'http')}://${process.env.HOST}:${process.env.PORT}`
 
 export default hostResolver
