3ef8f707f6c5e6b6fa10f9d7233947f35d09046b
Jon Linnell <linnell.jon@gmail.com>
2018-02-24 10:06:14 +0000

    Use nav-item/link instead of buttons

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
 
 import {
   faLink,
@@ -10,50 +10,63 @@ import {
   faSignOutAlt as faSignOut
 } from '@fortawesome/fontawesome-free-solid'
 
+import history from '../../history'
+
+const checkActivePage = (page) => {
+  const baseClasses = ['nav-link']
+  if (history.location.pathname === page) {
+    return [...baseClasses, 'active'].join(' ')
+  } else {
+    return baseClasses.join(' ')
+  }
+}
+
 class Navbar extends Component {
   render() {
     const navbarItemsAuthenticated = [
-      <LinkContainer to='/links' key='links'>
-        <button type='button' className='btn btn-secondary'>
+      <li className='nav-item' key='links'>
+        <Link className={checkActivePage('/links')} to='/links'>
           <FontAwesomeIcon icon={ faLink } className='mr-2' />
           Links
-        </button>
-      </LinkContainer>,
-      <LinkContainer to='/statistics' key='statistics'>
-        <button type='button' className='btn btn-secondary'>
+        </Link>
+      </li>,
+      <li className='nav-item' key='statistics'>
+        <Link className={checkActivePage('/statistics')} to='/statistics'>
           <FontAwesomeIcon icon={ faChartLine } className='mr-2' />
           Statistics
-        </button>
-      </LinkContainer>,
-      <LinkContainer to='/settings' key='settings'>
-        <button type='button' className='btn btn-secondary'>
+        </Link>
+      </li>,
+      <li className='nav-item' key='settings'>
+        <Link className={checkActivePage('/settings')} to='/settings'>
           <FontAwesomeIcon icon={ faCog } className='mr-2' />
           Settings
-        </button>
-      </LinkContainer>,
-      <button type='button' className='btn btn-secondary' key='logout' onClick={() => this.props.onLogout(this.props.history)} href='logout'>
-        <FontAwesomeIcon icon={ faSignOut } className='mr-2' />
-        Logout ({this.props.auth.user})
-      </button>
+        </Link>
+      </li>,
+      <li className='nav-item' key='onLogout'>
+        <a className='nav-link' href='#logout' onClick={() => this.props.onLogout(this.props.history)}>
+          <FontAwesomeIcon icon={ faSignOut } className='mr-2' />
+          Logout ({this.props.auth.user})
+        </a>
+      </li>
     ]
 
     const navbarItemsUnauthenticated = [
-      <LinkContainer to='/login' key='login'>
-        <button type='button' className='btn btn-secondary'>
+      <li className='nav-item' key='login'>
+        <Link className={checkActivePage('/login')} to='/login'>
           <FontAwesomeIcon icon={ faSignIn } className='mr-2' />
           Login
-        </button>
-      </LinkContainer>
+        </Link>
+      </li>
     ]
 
     return (
         <nav className='navbar navbar-expand-lg navbar-light'>
           <a className='navbar-brand'>Ricochet Web</a>
-            <div className='btn-group btn-group-sm ml-auto' role='group' aria-label='Navigation'>
+            <ul className='nav justify-content-end ml-auto' role='group' aria-label='Navigation'>
               {this.props.auth.isAuthenticated
                 ? navbarItemsAuthenticated
                 : navbarItemsUnauthenticated}
-            </div>
+            </ul>
         </nav>
     )
   }
