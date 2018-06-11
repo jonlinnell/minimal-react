15c0c45ff236808129b71132bbb12c79f73b936a
Jon Linnell <linnell.jon@gmail.com>
2018-06-11 17:04:29 +0100

    INCOMPLETE Navbar and Paper

--- src/components/Navbar/component.jsx
+++ src/components/Navbar/component.jsx
@@ -1,79 +1,87 @@
 import React from 'react'
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import { NavLink, Link } from 'react-router-dom'
 
-import {
-  faLink,
-  faChartLine,
-  faCog,
-  faSignInAlt as faSignIn,
-  faSignOutAlt as faSignOut,
-} from '@fortawesome/fontawesome-free-solid'
-
-import Hamburger from '../Hamburger'
+import Appbar from '@material-ui/core/Appbar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import blue from '@material-ui/core/colors/blue'
+import { withStyles } from '@material-ui/core/styles'
 
 import { navbarPropTypes, navbarDefaultProps } from '../../lib/propsValidation'
 
-import './styles.css'
+const styles = theme => ({
+  flex: {
+    flex: 1,
+  },
+  navItem: {
+    color: theme.palette.primary.contrastText,
+  },
+  navItemActive: {
+    color: blue[50],
+  },
+  navbar: {
+    marginBottom: theme.spacing.unit * 4,
+    backgroundColor: blue[900],
+  },
+  buttonActive: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+})
 
 const Navbar = (props) => {
+  const { classes } = props
+
   const navbarItemsAuthenticated = [
-    <li key="links">
-      <NavLink className="nav-link" activeClassName="nav-active" to="/links">
-        <FontAwesomeIcon icon={faLink} className="mr-2" />
+    <NavLink to="/links" key="links" className={classes.navItem} activeClassName={classes.navItemActive}>
+      <Button color="inherit">
         Links
-      </NavLink>
-    </li>,
-    <li key="statistics">
-      <NavLink className="nav-link" activeClassName="nav-active" to="/statistics">
-        <FontAwesomeIcon icon={faChartLine} className="mr-2" />
-        Statistics
-      </NavLink>
-    </li>,
-    <li key="settings">
-      <NavLink className="nav-link" activeClassName="nav-active" to="/settings">
-        <FontAwesomeIcon icon={faCog} className="mr-2" />
+      </Button>
+    </NavLink>,
+    <NavLink to="/statistics" key="statistics" className={classes.navItem} activeClassName={classes.navItemActive}>
+      <Button color="inherit">
+          Statistics
+      </Button>
+    </NavLink>,
+    <NavLink to="/settings" key="settings" className={classes.navItem} activeClassName={classes.navItemActive}>
+      <Button color="inherit">
         Settings
-      </NavLink>
-    </li>,
-    <li key="onLogout">
-      <a className="nav-link" href="#logout" onClick={() => props.onLogout()}>
-        <FontAwesomeIcon icon={faSignOut} className="mr-2" />
+      </Button>
+    </NavLink>,
+    <a href="#logout" key="onLogout" onClick={() => props.onLogout()} className={classes.navItem}>
+      <Button color="inherit">
         Logout ({props.auth.user})
-      </a>
-    </li>,
+      </Button>
+    </a>,
   ]
 
   const navbarItemsUnauthenticated = [
-    <li key="login">
-      <Link className="nav-link" href="/login" to="/login">
-        <FontAwesomeIcon icon={faSignIn} className="mr-2" />
+    <Link href="/login" to="/login" key="login" className={classes.navItem}>
+      <Button color="inherit">
         Login
-      </Link>
-    </li>,
+      </Button>
+    </Link>,
   ]
 
   return (
-    <div className="container-fluid p-0">
-      <div className="w-100 nav-bg">
-        <nav className="navbar navbar-expand-lg align-items-center navbar-dark col-sm-12 col-md-8 offset-md-2">
-          <div className="navbar-brand">Ricochet</div>
-          <Hamburger />
-          <div className="collapse navbar-collapse" id="navbar">
-            <ul className="navbar-nav justify-content-end ml-auto" role="group" aria-label="Navigation">
-              {props.auth.isAuthenticated
-                ? navbarItemsAuthenticated
-                : navbarItemsUnauthenticated}
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </div>
+    <Appbar position="static" className={classes.navbar}>
+      <Toolbar>
+        <Typography variant="title" color="inherit" className={classes.flex}>
+          Ricochet
+        </Typography>
+        {
+          props.auth.isAuthenticated
+            ? navbarItemsAuthenticated
+            : navbarItemsUnauthenticated
+        }
+      </Toolbar>
+    </Appbar>
   )
 }
 
 Navbar.propTypes = navbarPropTypes
 Navbar.defaultProps = navbarDefaultProps
 
-export default Navbar
+export default withStyles(styles)(Navbar)
 
