81d36cf4cf8b2ba3e76a334d5e533213dd43e814
Jon Linnell <linnell.jon@gmail.com>
2018-08-08 18:32:53 +0100

    Clean up dates in UserRow

--- src/components/UserRow/component.jsx
+++ src/components/UserRow/component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import leftPad from 'left-pad'
 
 import {
   faTimes,
@@ -8,6 +9,11 @@ import {
 
 import { userRowPropTypes } from '../../lib/propsValidation'
 
+const formatDate = (dateString) => {
+  const date = new Date(dateString)
+  return `${leftPad(date.getDate(), 2, '0')}-${leftPad(date.getMonth(), 2, '0')}-${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+}
+
 const UserRow = (props) => {
   const { onSetDeleteUser, onSetModifyUser } = props
   const { username, id, createdAt } = props.user
@@ -15,7 +21,7 @@ const UserRow = (props) => {
   return (
     <tr>
       <td>{username}</td>
-      <td>{createdAt}</td>
+      <td>{formatDate(createdAt)}</td>
       <td className="d-flex justify-content-end">
         <button
           className="btn btn-light"
